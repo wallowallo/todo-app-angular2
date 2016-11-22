4a20fb7e74b9c9cec586ce0f36745d6699652276
Krister <krister.torsvik@gmail.com>
2016-11-22 15:29:47 +0100

    mock todos with ngFor loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+export class Todo {
+  description: string;
+} 
+
+const TODOS: Todo[] = [
+  { description: 'Do homework' },
+  { description: 'Clean my room' },
+  { description: 'Do the dishes' },
+  { description: 'Go to the gym' }
+]; 
 
 @Component({
+  moduleId: 'module.id',
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Todo:';
+  todos = TODOS;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     FormsModule,
     HttpModule
   ],

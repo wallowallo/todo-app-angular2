9eb64e30fb951c725727f1c99e4d2eb8b47929a8
Krister <krister.torsvik@gmail.com>
2016-11-23 11:18:50 +0100

    added styles and worked on the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import {
+    FormBuilder,
+      FormControl,
+        FormGroup,
+} from '@angular/forms';
 
 export class Todo {
   title: string;
   description: string;
-} 
+}
 
 const TODOS: Todo[] = [
   { title: 'Do homework', description: 'Mathmatics' },
   { title: 'Clean my room', description: 'Stow away clothes' },
   { title: 'Hit the gym', description: 'Do chest and back exercises' },
-]; 
+];
 
 @Component({
   moduleId: 'module.id',
@@ -19,6 +24,23 @@ const TODOS: Todo[] = [
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Todo:';
+  header = 'Todo:';
   todos = TODOS;
+
+  addTodo: FormGroup;
+  title: FormControl;
+  description: FormControl;
+
+  constructor (builder: FormBuilder) {
+	  this.title = new FormControl('', []);
+	  this.description = new FormControl('', []);
+	  this.addTodo = builder.group({
+	  	title: this.title,
+	  	description: this.description
+  	});
+  }
+
+  login () {
+    console.log(this.addTodo.value);                                 
+ }
 }
